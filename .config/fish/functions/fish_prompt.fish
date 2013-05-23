10f5730a4bada40396e04201acfab00f8b7baab9
Pierre Carrier <pierre@gcarrier.fr>
2013-05-23 14:36:58 +0200

    fish_prompt.fish awesomeness

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,31 +1,41 @@
 function fish_prompt --description 'Write out the prompt'
   set stat $status
 
-  if not set -q __fish_prompt_hostname
-    set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
+  if not set -q __prompt_hostname
+    set -g __prompt_hostname (hostname|cut -d . -f 1)
+  end
+  
+  if not set -q __prompt_normal
+    set -g __prompt_normal (set_color normal)
+  end
+  
+  if not set -q __prompt_cwd
+    set -g __prompt_cwd (set_color $fish_color_cwd)
+  end
+  
+  if not set -q __prompt_error
+    set -g __prompt_error (set_color $fish_color_error)
   end
 
-  if not set -q __fish_prompt_normal
-    set -g __fish_prompt_normal (set_color normal)
+  if not set -q __prompt_vcs
+    set -g __prompt_vcs (set_color purple)
   end
 
-  switch $USER
-    case root
-      if not set -q __fish_prompt_cwd
-        if set -q fish_color_cwd_root
-          set -g __fish_prompt_cwd (set_color $fish_color_cwd_root)
-        else
-          set -g __fish_prompt_cwd (set_color $fish_color_cwd)
-        end
-      end
-
-      echo -n -s "$USER" @ "$__fish_prompt_hostname" ' ' "$__fish_prompt_cwd" (prompt_pwd) "$__fish_prompt_normal" '# '
-
-    case '*'
-      if not set -q __fish_prompt_cwd
-        set -g __fish_prompt_cwd (set_color $fish_color_cwd)
-      end
-      echo -n -s $stat ' ' "$USER" @ "$__fish_prompt_hostname" ' ' "$__fish_prompt_cwd" (prompt_pwd) "$__fish_prompt_normal" (vcprompt -f ' %b%m%u') '➜ '
+  if test $stat -ne 0
+    set precmd "$__prompt_error$stat$__prompt_normal "
+  else
+    set precmd
   end
+
+  set vcs_info (vcprompt -f '%b%m%u')
+
+  if test -n "$vcs_info"
+    set vcs_info " $__prompt_vcs$vcs_info$__prompt_normal"
+  end
+
+  echo -n -s $precmd \
+             "$USER" @ "$__prompt_hostname" ' ' \
+             "$__prompt_cwd" (prompt_pwd) "$__prompt_normal" \
+             "$vcs_info" '> '
 end
 
