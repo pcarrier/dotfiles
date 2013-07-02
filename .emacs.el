e408d054e3148be8f6233f0185c13978caec4f28
Pierre Carrier <pierre@gcarrier.fr>
2013-07-02 02:16:39 +0200

    .emacs.el: back to ido for all things

--- .emacs.el
+++ .emacs.el
@@ -4,28 +4,24 @@
       inhibit-startup-message t
       inhibit-startup-echo-area-message t
       tramp-default-method "ssh"
-      show-trailing-whitespace t
-      helm-idle-delay 0.1
-      helm-input-idle-delay 0.1
-      helm-quick-update t
+      ido-enable-flex-matching t
+      ido-everywhere t
+      ido-use-filename-at-point 'guess
+      ido-create-new-buffer 'always
+      ido-enable-prefix t
+      ido-enter-matching-directory 'only
       whitespace-style '(face
                          trailing tabs empty
                          space-after-tab space-after-tab
                          tab-mark)
       package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")
-                         ("melpa" . "http://melpa.milkbox.net/packages/"))
-      ;; Blame it on the HHKB, but feels awesome
-      mac-option-modifier 'super
-      mac-command-modifier 'meta)
+                         ("melpa" . "http://melpa.milkbox.net/packages/")))
 
 (require 'package)
 (package-initialize)
 
-(dolist (p '(helm
-             helm-ls-git
-             helm-descbinds
-             rainbow-delimiters
+(dolist (p '(rainbow-delimiters
              multiple-cursors
              paredit
              git-commit-mode
@@ -37,32 +33,27 @@
   (when (not (package-installed-p p))
     (package-install p)))
 
-(dolist (r '(helm-config
-             helm-ls-git
-             multiple-cursors
+(dolist (r '(multiple-cursors
              rainbow-delimiters
              paredit
-             whitespace))
+             whitespace
+             ido))
   (require r))
 
-(set-default-font "Inconsolata 16")
+(set-default-font "Inconsolata 14")
 ;;(load-theme 'wombat)
 
 (tool-bar-mode -1)
 (defalias 'yes-or-no-p 'y-or-n-p)
 
+(ido-mode 1)
 (global-whitespace-mode t)
-(helm-mode 1)
-(helm-descbinds-mode)
 (add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
 
 (global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
-(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
-(global-set-key (kbd "C-c h") 'helm-mini)
-(global-set-key (kbd "C-x f") 'helm-for-files)
 
 ;; Airbnb reserves tabs to Makefiles.
 (setq-default indent-tabs-mode nil)
